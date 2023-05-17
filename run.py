f5f8a25c609e5e61bf30241409646944f4c104e1
muadh hudji <muad.hudji@gmail.com>
2023-05-17 09:47:18 +0000

    Add function add_ships which inserts ships in the game board

--- run.py
+++ run.py
@@ -1,18 +1,32 @@
 from pprint import pprint
+from random import randint
 
 def board():
     """
     Creates game board
     """
     size = 5
-    game_board = []
+    empty_game_board = []
     for i in range(size):
         list = []
         for x in range(size):
             list.append(".")
-        game_board.append(list)    
+        empty_game_board.append(list)
+    game_board = add_ships(empty_game_board)    
     return game_board
 
+def add_ships(list):
+    """
+    Add 4 ships in the board by choosing a random index
+    """
+    ships = 4
+    while ships != 0:
+        x = randint(0,4)
+        y = randint(0,4)
+        if list.count("*") != 0:
+            list[x][y] = "*"
+            ships -= 1
+    return list
 
 def new_game():
     """
