a2886c1928a94ad6819ba47a3e39b33ed5e90621
muadh hudji <muad.hudji@gmail.com>
2023-05-17 09:00:20 +0000

    Add function board that creates game board

--- run.py
+++ run.py
@@ -1,3 +1,30 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from pprint import pprint
+
+def board():
+    """
+    Creates game board
+    """
+    size = 5
+    game_board = []
+    for i in range(size):
+        list = []
+        for x in range(size):
+            list.append(".")
+        game_board.append(list)    
+    return game_board
+
+
+def new_game():
+    """
+    Start a new game
+    """
+    player_board = board()
+    computer_board = board()
+    print("Player board")
+    pprint(player_board)
+    print("*"*28)
+    print("Computer board")
+    pprint(computer_board)
+
+
+new_game()
